e7e9c085d39bfe35491b99e3722aaa627bd889c8
Robert Misener <S00724017@acad.tri-c.edu>
2019-01-29 09:31:21 -0500

    Commiting after discovering error in original commit

---
+++ Lab2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        // GET: Products/Index/
+        public string Index()
+        {
+            string message =
+                HttpUtility.HtmlEncode("Product/Index is displayed");
+            return message;
+        }
+
+        // GET: Products/Browse
+        public string Browse()
+        {
+            string message =
+                HttpUtility.HtmlEncode("Product/Index is displayed");
+            return message;
+        }
+
+        // GET: Products/Details/105
+        public string Details(int id)
+        {
+            string message =
+                HttpUtility.HtmlEncode("Details displayed for Id = " + id);
+            return message;
+        }
+
+        // GET: Products/Location? zip = 44124
+        public string Location()
+        {
+            return "location displayed for " + Request.QueryString["zip"].ToString() + "44124";
+        }
+    }
+}
